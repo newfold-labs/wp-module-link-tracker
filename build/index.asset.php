85ea7ea63c80540fa6e6c3e79d0e28c651af5f00
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-07-22 21:29:52 +0100

    Tweak use a custom function to add query parameters while preserving the fragment

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,4 +1,1 @@
-<?php return array(
-	'dependencies' => array( 'wp-url' ),
-	'version'      => '2da82e1649655aa384fd',
-);
+<?php return array('dependencies' => array(), 'version' => '4e9a0ecaa9608899dafa');
